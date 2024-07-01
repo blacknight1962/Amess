dacee7133a825346cdf6c2dedca2e8f75d9007bf
blacknight1962 <sunugahn@gmail.com>
2024-07-01 21:12:14 +0900

    add debugging for DB connection

--- db.php
+++ db.php
@@ -1,12 +1,18 @@
-<?php
-$servername = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASSWORD");
-$database = getenv("DB_NAME");
+    <?php
+    $servername = getenv('DB_HOST');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $database = getenv('DB_NAME');
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+    // Debugging: 환경 변수 출력
+    error_log("DB_HOST: " . $servername);
+    error_log("DB_USER: " . $username);
+    error_log("DB_PASS: " . $password);
+    error_log("DB_NAME: " . $database);
 
-if (!$conn) {
-die("DB 접속 실패: " . mysqli_connect_error());
-}
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("DB 접속 실패: " . mysqli_connect_error());
+    }
 ?>
